131b33c1dc1f092334434c87d490ab28c8810d18
cstew527 <christopherstewart05@gmail.com>
2022-12-14 09:22:01 -0500

    fixed setters again

--- client/src/components/media/EditPost.js
+++ client/src/components/media/EditPost.js
@@ -61,15 +61,15 @@ const EditPost = () =>{
                 </div>
                 <div className="mb-3">
                     <label htmlFor="image" className="form-label">Image: </label>
-                    <input onChange={(event) => setComment(event.target.value)} value={image} type="text" className="form-control" id="image"/>
+                    <input onChange={(event) => setImage(event.target.value)} value={image} type="text" className="form-control" id="image"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="date" className="form-label">Date</label>
-                    <input onChange={(event) => setComment(event.target.value)} value={date} type="text" className="form-control" id="date"/>
+                    <input onChange={(event) => setDate(event.target.value)} value={date} type="text" className="form-control" id="date"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="location" className="form-label">Location</label>
-                    <input onChange={(event) => setComment(event.target.value)} value={location} type="text" className="form-control" id="location"/>
+                    <input onChange={(event) => setLocation(event.target.value)} value={location} type="text" className="form-control" id="location"/>
                 </div>
                 <button type="submit" className="btn btn-primary">Edit</button>
             </form>
